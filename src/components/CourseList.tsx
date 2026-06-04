27c60dfe36dfb664346ea110c3b64057442d6a67
fiorelliwong <fiorelliwong@gmail.com>
2026-06-03 19:06:34 -0500

    fetching data from given link

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -12,23 +12,23 @@ interface CourseListProps {
 }
 
 const CourseList = ({ courses }: CourseListProps) => {
-  return (
-    <div className="flex flex-wrap justify-center gap-4 p-4">
-      {Object.values(courses).map((course) => (
-        <div
-          key={course.number}
-          className="flex w-60 flex-col rounded-lg border border-gray-300 p-4 min-h-50 shadow-sm hover:shadow-md"
-        >
-          <h2 className="font-bold text-lg break-words">
-            {course.term} CS {course.number}
-          </h2>
-          <p className="text-gray-600 break-words">{course.title}</p>
-          <hr className="mt-auto border-gray-300" />
-          <p className="mt-2 text-sm text-gray-500 break-words">{course.meets}</p>
+    return (
+        <div className="flex flex-wrap justify-center gap-4 p-4">
+        {Object.values(courses).map((course) => (
+            <div
+            key={course.number}
+            className="flex w-60 flex-col rounded-lg border border-gray-300 p-4 min-h-50 shadow-sm hover:shadow-md"
+            >
+            <h2 className="font-bold text-lg break-words">
+                {course.term} CS {course.number}
+            </h2>
+            <p className="text-gray-600 break-words">{course.title}</p>
+            <hr className="mt-auto border-gray-300" />
+            <p className="mt-2 text-sm text-gray-500 break-words">{course.meets}</p>
+            </div>
+        ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default CourseList;
